4bd97031a0a784dd9596183bed674665ca605232
Nika Shamiladze <shamgoingzuck@gmail.com>
2025-04-01 01:27:09 +0400

    Update package version to 1.0.14 and modify tsup configuration to include additional entry point and build options for improved output management.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from "tsup";
+import { defineConfig, type Options } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
+export default defineConfig((flags) => {
+  const buildOptions: Options = {
+    minify: true,
+    clean: true,
+    format: ["cjs", "esm"],
+    dts: true,
+  };
+
+  return {
+    entry: ["src/index.ts", "src/types.ts"],
+    outDir: "lib",
+    ...buildOptions,
+  };
 });
